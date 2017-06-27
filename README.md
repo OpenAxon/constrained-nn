4cd316157232a7f7c1dc0453ab15280c2b0e9c99
Francois Scharffe <francois.scharffe@dextro.co>
2017-06-27 16:09:20 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Experiments towards adding logical constraints to multi-class, multi-label classifiers.
+- We specify constraints on the label ontology, and try to enforce these constraints on the network using multiple loss functions.
---
+++ generate_dataset.py
@@ -0,0 +1,29 @@
+from config import config
+import numpy as np
+import tensorflow as tf
+
+
+def generate_dataset_with_disjoint_classes(size, nb_disjoint, nb_others):
+    ''' Generates a synthetic dataset for testing disjoint classes restrictions
+    @param:size: the number of instances in the dataset
+    @param:nb_disjoint: the number of disjoint classes to be generated
+    @param:nb_others: the number of other classes to be generated'''
+    np.random.seed = config['dataset_seed']
+    x_disjoint = np.random.rand(size, nb_disjoint)
+    y_disjoint = np.zeros((size, nb_disjoint))
+    max_indices = np.argmax(x_disjoint, axis=1)
+    I = np.indices(max_indices.shape)
+    y_disjoint[I, max_indices] = 1
+    x = np.random.rand(size, nb_others)
+    y = (x > 0.5).astype(np.float)
+    return np.hstack((x_disjoint, x)), np.hstack((y_disjoint, y))
+
+
+def split(X, Y, test_size):
+    return X[int(X.shape[0]*test_size):], Y[int(Y.shape[0]*test_size):], \
+           X[:int(X.shape[0]*test_size)], Y[:int(Y.shape[0]*test_size)]
+
+
+def generate_and_split(size, nb_disjoint, nb_others, test_size):
+    X, Y = generate_dataset_with_disjoint_classes(size, nb_disjoint, nb_others)
+    return split(X, Y, test_size)
