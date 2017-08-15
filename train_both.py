c36fc70e3f9abacd3eb84f2d21aacd8cbabb30d4
Francois Scharffe <francois.scharffe@dextro.co>
2017-08-15 16:14:02 -0700

    simplifying code

--- train_both.py
+++ train_both.py
@@ -6,6 +6,7 @@ from keras import backend as K
 from keras.activations import softmax
 from keras.layers import Dense, Dropout, Input, concatenate
 from keras.losses import categorical_crossentropy, mean_squared_error, binary_crossentropy
+from keras.metrics import binary_accuracy, categorical_accuracy
 from keras.models import Model
 from keras.utils import plot_model
 from math import log
@@ -14,8 +15,8 @@ import os
 import tensorflow as tf
 import uuid
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-KERAS_VERBOSITY = 0
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+KERAS_VERBOSITY = 2
 
 def mutually_exclusive_loss(y_true, y_pred):
     '''define a loss over a set of label. 
@@ -46,14 +47,20 @@ def model(input_shape, nb_output):
     plot_model(model, to_file='images/constrained-network.png', show_shapes=True)
     return(model)
 
-def constraint_metric(y_true, y_pred):
+def constraint_accuracy(y_true, y_pred):
     y_disjoint_true = y_true[:, :config['nb_disjoint_classes']]
     y_disjoint_pred = y_pred[:, :config['nb_disjoint_classes']]
-    return tf.equal(tf.cast(y_disjoint_pred, tf.bool), tf.cast(y_disjoint_true, tf.bool))
+#    return tf.reduce_all(tf.equal(tf.cast(tf.cast(y_disjoint_pred+0.5, tf.int32), tf.bool), tf.cast(y_disjoint_true, tf.bool)))
+    return categorical_accuracy(y_disjoint_true, y_disjoint_pred)
+
+def others_accuracy(y_true, y_pred):
+    y_others_true = y_true[:, config['nb_disjoint_classes']:]
+    y_others_pred = y_pred[:, config['nb_disjoint_classes']:]
+    return binary_accuracy(y_others_true, y_others_pred)
 
 def train_simple(model, x, y, tboard):
     model.compile(optimizer='adam', 
-                  metrics=[constraint_metric],
+                  metrics=[constraint_accuracy, others_accuracy],
                   loss={'main_output': binary_crossentropy})
     model.fit(x, # data
               y, # labels
@@ -65,7 +72,7 @@ def train_simple(model, x, y, tboard):
 
 def train_constrained(model, x, y, tboard):
     model.compile(optimizer='adam', 
-                  metrics=[constraint_metric],
+                  metrics=[constraint_accuracy, others_accuracy],
                   loss={'main_output': multiple_loss})
     model.fit(x,
               y, # labels
@@ -88,60 +95,20 @@ def main():
     # print(m.output_shape)
     # print(x_train.shape)
     # print(y_train.shape)
+
+#    print("Network type: simple")
+#    name = NAME
+#    net_name = name+str(uuid.uuid4())
+#    tboard = os.path.join(config['logdir_path'], net_name)
+#    train_simple(m, x_train, y_train, tboard)
+
     print("Network type: constrained")
     name = NAME+"_DISJOINT_WEIGHT_{}_".format(config['disjoint_classes_output_weight'])
     net_name = name+str(uuid.uuid4())
     tboard = os.path.join(config['logdir_path'], net_name)
     train_constrained(m, x_train, y_train, tboard)
-    total_loss, disjoint_classes_loss, main_loss = test(m, x_test, y_test)
-    print("Total loss:{}\nMain loss:{}\nDisjoint classes loss:{}".format(total_loss, main_loss, disjoint_classes_loss))
-    wrong_pred = [0, 0]
-    incorrects = 0
-    pred_data = []
-    for i in range(len(x_test)):
-        predictions = m.predict(np.reshape(x_test[i], (1, x_test[i].shape[0])), batch_size=1)
-        binary_pred = [np.zeros(config['nb_disjoint_classes']), np.zeros(config['nb_other_classes'])]
-        binary_pred[0][np.argmax(predictions[0][0])] = 1
-        binary_pred[1] = (predictions[1][0] > 0.5).astype(np.float)
-        pred_data.append({'prediction': list(np.concatenate(binary_pred)), 'truth': list(y_test[i])})
-        incorrect = False
-        k = 0
-        for j in range(len(binary_pred[k])):
-            if binary_pred[k][j] != y_test[i][j+(k*config['nb_disjoint_classes'])]:
-                wrong_pred[k] += 1
-#                print("{}\ninput:{}\npred:{}\npred_binary:{}\ntruth:      {}\n---------------------"\
-#                      .format(k, x_test[i], predictions, binary_pred, y_test[i]))
-                incorrect = True
-                break
-        k = 1
-        for j in range(len(binary_pred[k])):
-            if binary_pred[k][j] != y_test[i][j+(k*config['nb_disjoint_classes'])]:
-                wrong_pred[k] += 1
-#                print("{}\ninput:{}\npred:{}\npred_binary:{}\ntruth:      {}\n---------------------"\
-#                      .format(k, x_test[i], predictions, binary_pred, y_test[i]))
-                incorrect = True
-        if incorrect:
-            incorrects += 1
-
-
-    acc = (len(x_test)-incorrects)/float(len(x_test))
-    print("prediction accuracy: {}".format(acc))
-    print("Number of times the disjointness constraint was violated: {}/{}".format(wrong_pred[0], len(x_test)))
-    print("Generic classes classificaton accuracy: {}/{}".format(wrong_pred[1]/float(config['nb_other_classes']), len(x_test)))
-    payload = {'network_type': "constrained",
-               'config': config,
-               'results': {'strict_accuracy': acc,
-                        'nb_disjoint_constraint_broken': wrong_pred[0],
-                        'nb_other_incorrect': wrong_pred[1],
-                        'loss': total_loss,
-                        'disjoint_classes_loss': disjoint_classes_loss,
-                        'other_classes_loss': main_loss },
-               'tboard_log_file': tboard,
-               'predictions': pred_data }
-
-    with open(os.path.join(config['results_path'], net_name), "wt") as f:
-        json.dump(payload, f)
 
 if __name__ == '__main__':
     main()
+
 
